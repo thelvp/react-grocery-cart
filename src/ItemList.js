4d7047e5b0d73af00b587c10d9a8d946359ebd68
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-04-25 14:00:29 +0200

    Small typo fix

--- src/ItemList.js
+++ src/ItemList.js
@@ -5,11 +5,12 @@ export default function ItemList({ items, onItemClick }) {
     const item = target.value;
     onItemClick(item);
   };
+
   return (
     <div className="itemList">
       {items.map((item, index) => (
         <button value={item} onClick={handleClick} key={index} className="itemBtn">
-          {item} {item.amount}
+          {item}
         </button>
       ))}
     </div>
